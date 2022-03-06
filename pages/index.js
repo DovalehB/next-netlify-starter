4097b5c47f828740c6baa9bccb1c61b6509f6d0a
DovalehB <70105106+DovalehB@users.noreply.github.com>
2022-03-06 15:15:46 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome again to my New Home!" />
+        <Header title="This is Dov Barak's Home Page!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
